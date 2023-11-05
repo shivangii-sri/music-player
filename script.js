ce3d723b03c020cdf48d6fafb1aaebaaac775605
shivangii-sri <shivangisrivastava762@gmail.com>
2023-11-06 02:24:34 +0530

    Logic to jump in middle of progress bar

--- script.js
+++ script.js
@@ -130,8 +130,19 @@ function updateProgressBar(e) {
     }
 }
 
+function setProgressBar(e){
+    console.log(e);
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = music ;
+    music.currentTime = (clickX  / width) * duration;
+
+}
+
 
 // Event Listeners
 prevBtn.addEventListener('click' , prevSong);
 nextBtn.addEventListener('click' , nextSong);
 music.addEventListener('timeupdate' , updateProgressBar);
+progressContainer.addEventListener('click' , setProgressBar);
+music.addEventListener('ended' , nextSong);
